97eb855fc5ea9b39e1703e97c61edc85ac886d6a
fedi <fadikossentini@insat.u-carthage.tn>
2022-03-19 15:27:20 +0100

    responsive done

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,20 +12,24 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              Text("No transactions added yet!"),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'assets/fonts/images/waiting.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
+        ? LayoutBuilder(
+            builder: ((context, constraints) {
+              return Column(
+                children: [
+                  Text("No transactions added yet!"),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/fonts/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            }),
           )
         : ListView.builder(
             itemBuilder: (ctx, index) {
@@ -51,11 +55,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMEd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          textColor: Theme.of(context).errorColor,
+                          label: Text("Delete"),
+                          onPressed: () => deleteTx(transactions[index].id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
